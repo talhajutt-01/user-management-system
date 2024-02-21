cdf87b4bbef207a3d08aa6c04c6a79682363a637
talhajutt-01 <talhaj928@gmail.com>
2024-02-21 18:17:30 +0500

    commit

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -39,20 +39,7 @@ exports.homepage = async (req, res) => {
     console.log(error);
   }
 };
-// exports.homepage = async (req, res) => {
-//     const messages = await req.consumeFlash('info');
-//     const locals = {
-//       title: 'NodeJs',
-//       description: 'Free NodeJs User Management System'
-//     }
 
-//     try {
-//       const customers = await Customer.find({}).limit(22);
-//       res.render('index', { locals, messages, customers } );
-//     } catch (error) {
-//       console.log(error);
-//     }
-// }
 
 /**
  * GET /
